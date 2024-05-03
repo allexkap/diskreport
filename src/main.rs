6539dd8487f2d25e200bfc5aaa5f2df51fd43f16
allexkap <aekpstn@gmail.com>
2024-05-03 15:51:24 +0300

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
 use std::env;
 use std::path::Path;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() == 2);
     let root_path = Path::new(&args[1]);
 
-    let total = walker(root_path);
-    println!("Total of '{}': {:.2} Mb", root_path.display(), total as f64 / f64::powf(2.0, 20.0));
+    match walker(root_path) {
+        Ok(total) => println!("Total of '{}': {:.2} bytes", root_path.display(), total),
+        Err(error) => println!("Error: {}", error),
+    }
 }
 
-fn walker(root_path: &Path) -> u64 {
+fn walker(root_path: &Path) -> Result<u64, Box<dyn Error>>  {
     let mut total = 0;
-    for entry in root_path.read_dir().unwrap() {
-        let path = entry.unwrap().path();
-        if path.is_dir() {
-            total += walker(&path);
-        }
-        else {
-            println!("{:?} {:?}", path, path.metadata().unwrap().len());
-            total += path.metadata().unwrap().len();
+    for entry in root_path.read_dir()? {
+        match entry {
+            Ok(entry) => {
+                let path = entry.path();
+                total += if path.is_dir() {
+                    walker(&path).unwrap_or_else(|e| {
+                        println!("{}: {}", path.display(), e);
+                        0
+                    })
+                } else {
+                    match path.metadata() {
+                        Ok(data) => data.len(),
+                        Err(error) => {
+                            println!("{}: {}", path.display(), error);
+                            0
+                        }
+                    }
+                };
+            },
+            Err(error) => println!("{:?}", error),
         }
     }
-    total
+    Ok(total)
 }
