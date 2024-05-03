89d6119de8baf31a13bfa467ee97217f31733989
allexkap <aekpstn@gmail.com>
2024-05-03 14:14:40 +0300

    add walker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2);
+    let root_path = Path::new(&args[1]);
+
+    let total = walker(root_path);
+    println!("Total of '{}': {:.2} Mb", root_path.display(), total as f64 / f64::powf(2.0, 20.0));
+}
+
+fn walker(root_path: &Path) -> u64 {
+    let mut total = 0;
+    for entry in root_path.read_dir().unwrap() {
+        let path = entry.unwrap().path();
+        if path.is_dir() {
+            total += walker(&path);
+        }
+        else {
+            println!("{:?} {:?}", path, path.metadata().unwrap().len());
+            total += path.metadata().unwrap().len();
+        }
+    }
+    total
 }
