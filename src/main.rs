1b08a759544882e258cf8d67595c415935fa33a7
allexkap <aekpstn@gmail.com>
2024-05-06 16:17:04 +0300

    add struct

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,70 @@
 use std::env;
+use std::error::Error;
 use std::path::Path;
-use std::thread;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() == 2);
     let root_path = Path::new(&args[1]);
 
-    println!(
-        "Total of '{}': {:.2} bytes",
-        root_path.display(),
-        walker(root_path, true)
-    )
+    match walker(root_path) {
+        Ok(data) => {
+            let size = data.iter().map(|e| e.size).sum::<u64>();
+            println!("{} Mb", size / 1048576);
+        }
+        Err(error) => eprintln!("{}: {}", root_path.display(), error),
+    }
 }
 
-fn walker(root_path: &Path, thread_mode: bool) -> u64 {
-    let mut total = 0;
-    let mut threads = if thread_mode { Some(Vec::new()) } else { None };
+#[derive(Debug)]
+struct Entry {
+    name: String,
+    size: u64,
+    dir: Option<Vec<Entry>>,
+}
+
+fn walker(root_path: &Path) -> Result<Vec<Entry>, Box<dyn Error>> {
     match root_path.read_dir() {
         Ok(dir) => {
+            let mut content = Vec::new();
             for entry in dir {
                 match entry {
                     Ok(entry) => {
                         let path = entry.path();
-                        total += if path.is_dir() {
-                            if thread_mode {
-                                let th = thread::spawn(move || walker(&path, false));
-                                threads.as_mut().unwrap().push(th);
-                                0
-                            } else {
-                                walker(&path, false)
+                        let Ok(name) = path
+                            .file_name()
+                            .unwrap_or(path.as_os_str())
+                            .to_owned()
+                            .into_string()
+                        else {
+                            eprintln!("Invalid Unicode data: {}", path.display());
+                            continue;
+                        };
+                        if path.is_dir() {
+                            match walker(&path) {
+                                Ok(inner_dir) => content.push(Entry {
+                                    name,
+                                    size: inner_dir.iter().map(|e| e.size).sum(),
+                                    dir: Some(inner_dir),
+                                }),
+                                Err(error) => eprintln!("{}: {}", error, path.display()),
                             }
                         } else {
                             match path.metadata() {
-                                Ok(data) => data.len(),
-                                Err(error) => {
-                                    println!("{}: {}", path.display(), error);
-                                    0
-                                }
+                                Ok(meta) => content.push(Entry {
+                                    name,
+                                    size: meta.len(),
+                                    dir: None,
+                                }),
+                                Err(error) => eprintln!("{}: {}", error, path.display()),
                             }
-                        };
+                        }
                     }
-                    Err(error) => println!("{:?}", error),
+                    Err(error) => eprintln!("{}: {}", error, root_path.display()),
                 }
             }
+            Ok(content)
         }
-        Err(error) => println!("{:?}", error),
+        Err(err) => Err(err.into()),
     }
-    if thread_mode {
-        for thread in threads.unwrap() {
-            total += thread.join().unwrap();
-        }
-    }
-    total
 }
