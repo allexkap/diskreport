a80dc492fbb1e359adb834b62d62fd92d2f3331b
allexkap <aekpstn@gmail.com>
2024-05-10 17:26:55 +0300

    add sectors description

--- src/gui.py
+++ src/gui.py
@@ -1,3 +1,4 @@
+import math
 import tkinter as tk
 
 
@@ -8,6 +9,10 @@ def hidh_dpi_fix():
         from ctypes import windll
 
         windll.shcore.SetProcessDpiAwareness(1)
+
+
+def eqgt(a, b):
+    return 0 if a < b else 1 if a == b else 2
 
 
 class PieDiagram(tk.Frame):
@@ -21,43 +26,63 @@ class PieDiagram(tk.Frame):
     }
 
     def __init__(self, *args, values, **kwargs):
-        self.length = 400
-        self.offset = 20
+        self.sizes = (1000, 800)
         tk.Frame.__init__(self, *args, **kwargs)
         self.canvas = tk.Canvas(
-            width=self.length,
-            height=self.length,
+            width=self.sizes[0],
+            height=self.sizes[1],
             background=self.colors['background'],
         )
         self.canvas.pack()
+        self.canvas.bind('<Button-1>', self.on_click)
         self.draw(values)
 
     def draw(self, values):
         total = sum(values)
         values = sorted(values, reverse=True)
-        sectors = [value / total * 360 for value in values[:9]]
-        if len(values) >= 9:
-            sectors.append(sum(values[9:]) / total * 360)
+        sectors = [value / total * 360 for value in values]
+
+        radius = min(10**6, self.sizes[0] * 0.3)
+        center = [self.sizes[0] / 2, self.sizes[1] / 2]
+        center[1] += self.sizes[0] * 0.05
+        pie_box = (
+            center[0] - radius,
+            center[1] - radius,
+            center[0] + radius,
+            center[1] + radius,
+        )
 
         angle, dir = 90, -1
         for i, sector in enumerate(sectors):
             sector *= dir
             self.canvas.create_arc(
-                self.offset,
-                self.offset,
-                self.length - self.offset,
-                self.length - self.offset,
+                *pie_box,
                 start=angle,
                 extent=sector,
-                fill=self.colors['palette'][i],
+                fill=self.colors['palette'][i % len(self.colors['palette'])],
                 outline=self.colors['border'],
             )
+
+            text_angle = angle + sector / 2
+            anchor = ('e', 'center', 'w')[eqgt((text_angle - 90) % 360, 180)]
+            self.canvas.create_text(
+                center[0] + math.cos(math.radians(text_angle)) * (radius + 10),
+                center[1] - math.sin(math.radians(text_angle)) * (radius + 10),
+                anchor=anchor,
+                text='Divinity Original Sin 10',
+            )
             angle += sector
+
+    def on_click(self, event):
+        n = self.canvas.find_overlapping(event.x, event.y, event.x, event.y)
+        if n:
+            print(n)
 
 
 if __name__ == '__main__':
     hidh_dpi_fix()
     root = tk.Tk()
     root.resizable(False, False)
-    pie = PieDiagram(root, values=(1, 3, 2, 1)).pack()
+    root.bind('<Escape>', lambda e: exit())
+    pie = PieDiagram(root, values=[1] * 9).pack()
     root.mainloop()
