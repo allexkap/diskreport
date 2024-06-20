9404b66a85dfe6e07cf6edb68c2a7f66e4efde4b
allexkap <aekpstn@gmail.com>
2024-06-20 04:07:01 +0300

    add gui demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use eframe::egui;
 use std::env;
 use std::path::Path;
 
@@ -6,11 +7,56 @@ fn main() {
     assert!(args.len() == 2);
     let root_path = Path::new(&args[1]);
 
-    match ReportEntry::build(root_path) {
+    let report = match ReportEntry::build(root_path) {
         Ok(data) => {
             println!("{}: {} Mb", data.name, data.size / u64::pow(2, 20));
+            data
         }
-        Err(error) => eprintln!("{}: {}", root_path.display(), error),
+        Err(error) => {
+            eprintln!("{}: {}", root_path.display(), error);
+            return;
+        }
+    };
+
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Disk Report",
+        options,
+        Box::new(|_cc| Box::new(DiskReportApp { dir: report })),
+    )
+    .unwrap();
+}
+
+struct DiskReportApp {
+    dir: ReportEntry,
+}
+
+impl DiskReportApp {
+    fn add_collapsing(ui: &mut egui::Ui, entry: &ReportEntry) {
+        if entry.dir_entries.is_some() {
+            ui.collapsing(entry.name.clone(), |ui| {
+                entry
+                    .dir_entries
+                    .as_ref()
+                    .unwrap()
+                    .iter()
+                    .for_each(|d| DiskReportApp::add_collapsing(ui, &d))
+            });
+        } else {
+            ui.label(entry.name.clone());
+        }
+    }
+}
+
+impl eframe::App for DiskReportApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            DiskReportApp::add_collapsing(ui, &self.dir);
+        });
     }
 }
 
@@ -60,6 +106,11 @@ fn walker(dir_path: &Path) -> Vec<ReportEntry> {
             }
         })
     }
-    content.sort_by_key(|e| std::cmp::Reverse(e.size));
+    content.sort_by(|a, b| {
+        b.dir_entries
+            .is_some()
+            .cmp(&a.dir_entries.is_some())
+            .then(b.size.cmp(&a.size))
+    });
     content
 }
